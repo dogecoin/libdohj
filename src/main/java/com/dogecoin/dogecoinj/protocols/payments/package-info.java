39fe02389b87f11d9a0e39c9b91507ae3eacb7ce
Ross Nicoll <jrn@jrn.me.uk>
2015-08-08 22:47:46 +0100

    Add Dogecoin payment protocol support
    Add Dogecoin payment protocol support, and remove unused proto definition
    files.

---
+++ src/main/java/com/dogecoin/dogecoinj/protocols/payments/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The BIP70 payment protocol wraps Bitcoin transactions and adds various useful features like memos, refund addresses
+ * and authentication.
+ */
+package com.dogecoin.dogecoinj.protocols.payments;
